6e52e38da6b76fdd5b6393d9d52e81269731aa71
Lan Le <l324018852@outlook.com>
2023-12-24 18:23:30 -0500

    Create add project form

--- src/index.js
+++ src/index.js
@@ -12,6 +12,39 @@ let projects = [
     {id: 3, name: "Personal"}
 ]
 const projectsContainerDiv = document.querySelector(".projects__list");
+const addNewProjectsDiv = document.querySelector(".projects__add__button");
+const projectsNewProjectFormDiv = document.querySelector(".projects__new-project-form");
+const projectsFormCancelButton = document.querySelector(".projects__new-project-form__button--cancel");
+const projectsFormAddButton = document.querySelector(".projects__new-project-form__button--create");
+const projectsProjectTextInput = document.querySelector(".projects__new-project-form__text-input");
+
+addNewProjectsDiv.addEventListener("click", (e) => {
+    addNewProjectsDiv.style.display = "none";
+    projectsNewProjectFormDiv.style.display = "block"; 
+});
+
+projectsFormCancelButton.addEventListener("click", (e) => {
+    addNewProjectsDiv.style.display = "block";
+    projectsNewProjectFormDiv.style.display = "none";
+});
+
+projectsFormAddButton.addEventListener("click", (e) => {
+    console.log(projectsProjectTextInput.value);
+    addNewProjectsDiv.style.display = "block";
+    projectsNewProjectFormDiv.style.display = "none";
+
+    let newProject = createProject(projectsProjectTextInput.value);
+    projects.push(newProject);
+    projectsProjectTextInput.value = "";
+    
+    render();
+    console.log(projects);
+});
+
+let createProject = (name) => {
+    return {id: Date.now().toString(), name: name, tasks: []};
+}
+
 
 function render() {
     clearElement(projectsContainerDiv);
@@ -34,8 +67,6 @@ function render() {
 }
 
 
-
-
 function clearElement(element) {
     if (element == null) {
         return false;
@@ -43,8 +74,9 @@ function clearElement(element) {
 
     while (element.firstChild) {
         element.firstChild.remove();
-        return true;
     }
+    
+    return true;
 }
 
 
