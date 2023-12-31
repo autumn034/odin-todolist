1c37e331ac5e4ddeb782ce27e45b8cfb932284f3
Lan Le <l324018852@outlook.com>
2023-12-31 14:27:31 -0500

    Add delete project function

--- src/index.js
+++ src/index.js
@@ -8,37 +8,52 @@ const createProjectForm = document.querySelector("#createProjectForm");
 const createProjectSubmitButton = document.querySelector("#createProjectSubmitButton");
 const createProjectCancelButton = document.querySelector("#createProjectCancelButton");
 const createProjectTextInput = document.querySelector("#createProjectTextInput");
-
-const projectsTemplate = [
-    {id: 1, name: "Work"}, 
-    {id: 2, name: "School"}, 
-    {id: 3, name: "Personal"}
-];
+const taskContainer = document.querySelector("#taskContainer");
+const activeProjectHeader = document.querySelector("#activeProjectHeader");
+const deleteProjectButton = document.querySelector("#deleteProjectButton");
+const mainContainer = document.querySelector("#main");
 
 let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECTS_KEY)) || projectsTemplate;
 let activeProjectId = JSON.parse(localStorage.getItem(LOCAL_STORAGE_ACTIVE_PROJECT_ID_KEY)) || 1;
 
-
-class Task {
-    constructor (name, due) {
+class Task 
+{
+    constructor (name, due) 
+    {
         this.name = name;
         this.due = due;
         this.doneStatus = false;
     }
 }
 
-projectsContainerDiv.addEventListener("click", e => {
+
+const projectsTemplate = [
+    {id: 1, name: "Work", tasks: [new Task("Test1", Date.now().toString())] }, 
+    {id: 2, name: "School", tasks: [new Task("Test2", Date.now().toString())]}, 
+    {id: 3, name: "Personal", tasks: [new Task("Test3", Date.now().toString())]}
+];
+
+
+projectsContainerDiv.addEventListener("click", e => 
+{
+    // prevents tasks from disappearing when clicking on whitespaces within the project container
+    if (!(e.target.classList.contains("projects__button")))
+    {
+        return;
+    }
+
     activeProjectId = e.target.dataset.projectId;
     saveAndRender();
 });
 
-
-createProjectButton.addEventListener("click", (e) => {
+createProjectButton.addEventListener("click", (e) => 
+{
     createProjectButton.style.display = "none";
     createProjectForm.style.display = "block"; 
 });
 
-createProjectCancelButton.addEventListener("click", (e) => {
+createProjectCancelButton.addEventListener("click", (e) => 
+{
     createProjectButton.style.display = "block";
     createProjectForm.style.display = "none";
 });
@@ -48,7 +63,8 @@ createProjectSubmitButton.addEventListener("click", (e) => {
     createProjectForm.style.display = "none";
     let createProjectTextInputValue = createProjectTextInput.value;
 
-    if (createProjectTextInputValue == null || createProjectTextInputValue == "") {
+    if (createProjectTextInputValue == null || createProjectTextInputValue == "") 
+    {
         alert("Blank value cannot be used for project name.");
         return
     }
@@ -57,6 +73,13 @@ createProjectSubmitButton.addEventListener("click", (e) => {
     projects.push(newProject);
     createProjectTextInput.value = "";
     
+    saveAndRender();
+});
+
+deleteProjectButton.addEventListener("click", () => 
+{
+    projects = projects.filter((project) => project.id != activeProjectId);
+    activeProjectId = null;
     saveAndRender();
 });
 
@@ -76,13 +99,24 @@ let createProject = (name) => {
 
 function render() {
     clearElement(projectsContainerDiv);
+    clearElement(taskContainer);
+
+    if (activeProjectId == null) 
+    {
+        mainContainer.style.display = "none";
+    }
+    else
+    {
+        mainContainer.style.display = "";
+    }
 
     projects.forEach(project => {
         let projectButton = document.createElement("button");
         projectButton.classList.add("projects__button");
       
         if (activeProjectId == project.id) 
-        {
+        {            
+            activeProjectHeader.textContent = project.name;
             projectButton.classList.add("projects__button--disable");
         }
 
@@ -93,7 +127,7 @@ function render() {
     });
 }
 
-function renderMain() {
+function renderTasks(projectId) {
 
 }
 
